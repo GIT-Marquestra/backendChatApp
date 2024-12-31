bd3f36ce70cf0580fc3a8f94ceda88c36591de9d
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Pro-4.local>
2024-12-31 15:13:00 +0530

    its done

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_USER_PASS = void 0;
-const { Router } = require("express");
-exports.JWT_USER_PASS = process.env.JWT_USER_PASS;
-// module.exports = {
-//     JWT_USER_PASS
-// }
